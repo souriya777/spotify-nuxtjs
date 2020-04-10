c3ee9125dbab6bf3209fff0a6fe2504f5b009b51
Souriya Phongsavanh <souriya.phongsavanh777@gmail.com>
2020-04-10 15:10:37 +0200

    implement 1st to to access spotify token

--- pages/albums.vue
+++ pages/albums.vue
@@ -1,18 +1,5 @@
 <template>
   <div>
     <h1>album</h1>
-    <p>code: {{ spotifyCode }}</p>
   </div>
 </template>
-<script>
-export default {
-  data() {
-    return {
-      spotifyCode: ''
-    }
-  },
-  mounted() {
-    this.spotifyCode = this.$route.query.code
-  }
-}
-</script>
--- pages/login.vue
+++ pages/login.vue
@@ -2,32 +2,106 @@
   <div>
     <h1>Login</h1>
     <button @click="login">Authorize</button>
+    <!-- <button v-if="spotifyCode" @click="getToken">Get token</button> -->
+    <p>code: {{ spotifyCode }}</p>
+    <p>token: {{ spotifyToken }}</p>
+    <div v-if="spotifyCode" class="code">
+      <code>{{ curl }}</code>
+    </div>
   </div>
 </template>
 <script>
+// import axios from 'axios'
+
+const AUTHORIZE_ENDPOINT = 'https://accounts.spotify.com/authorize'
+const TOKEN_ENDPOINT = 'https://accounts.spotify.com/api/token'
+
 export default {
+  fetch() {
+    this.login()
+  },
   data() {
     return {
-      url: '',
+      spotifyCode: '',
+      spotifyToken: '',
       spotifyClientId: process.env.NUXT_ENV_SPOTIFY_CLIENT_ID,
       spotifyClientSecret: process.env.NUXT_ENV_SPOTIFY_CLIENT_SECRET,
       spotifyRedirectUri: process.env.NUXT_ENV_SPOTIFY_REDIRECT_URI,
       spotifyScopes: process.env.NUXT_ENV_SPOTIFY_SCOPES
     }
   },
+  computed: {
+    curl() {
+      return `
+          curl -d grant_type=authorization_code -d code=${this.spotifyCode} -d redirect_uri=${this.spotifyRedirectUri} -d client_id=${this.spotifyClientId} -d client_secret=${this.spotifyClientSecret} ${TOKEN_ENDPOINT}
+        `
+    }
+  },
+  mounted() {
+    this.spotifyCode = this.$route.query.code
+    this.getToken()
+  },
   methods: {
     login() {
-      const authorizeEndpoint = 'https://accounts.spotify.com/authorize'
-
-      const url = `${authorizeEndpoint}?response_type=code&client_id=${
+      const url = `${AUTHORIZE_ENDPOINT}?response_type=code&client_id=${
         this.spotifyClientId
       }&scope=${encodeURIComponent(
         this.spotifyScopes
       )}&redirect_uri=${encodeURIComponent(this.spotifyRedirectUri)}`
 
-      this.url = url
       window.location = url
+    },
+    getToken() {
+      const authOptions = {
+        url: TOKEN_ENDPOINT,
+        form: {
+          code: this.spotifyCode,
+          redirect_uri: this.spotifyRedirectUri,
+          grant_type: 'authorization_code'
+        },
+        headers: {
+          Authorization:
+            'Basic ' +
+            Buffer.from(
+              this.spotifyClientId + ':' + this.spotifyClientSecret
+            ).toString('base64')
+        },
+        json: true
+      }
+      this.$axios(authOptions, (error, response, body) => {
+        if (error) {
+          this.spotifyToken = 'errorrrr'
+        }
+        this.spotifyToken = response
+      })
+      // const bearer = Buffer.from(
+      //   this.spotifyClientId + ':' + this.spotifyClientSecret
+      // ).toString('base64')
+      // const response = await this.$axios.$post(
+      //   TOKEN_ENDPOINT,
+      //   {
+      //     grant_type: 'authorization_code',
+      //     code: this.spotifyCode,
+      //     redirect_uri: this.spotifyRedirectUri
+      //   },
+      //   {
+      //     headers: {
+      //       Accept: 'application/json',
+      //       Authorization: `Basic ${bearer}`,
+      //       'Content-Type': 'application/x-www-form-urlencoded'
+      //     }
+      //   }
+      // )
+      // this.spotifyToken = response
     }
   }
 }
 </script>
+<style>
+.code {
+  margin: 2rem;
+  color: #222;
+  font-size: 1.6rem;
+  background-color: #fff;
+}
+</style>
